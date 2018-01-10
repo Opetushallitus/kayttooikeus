54f6f53adbf148913c87c7598942137948f59ff2
Tapio Jaakkola <tapio.jaakkola@github.mail.kapsi.fi>
2018-01-10 10:41:48 +0200

    KJHH-1226: Add note about the change

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/controller/CasController.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/controller/CasController.java
@@ -121,7 +121,7 @@ public class CasController {
         Map<String, String> queryParams;
         if (StringUtils.hasLength(kutsuToken)) {
             try {
-                // Dekoodataan etunimet ja sukunimi käsin
+                // Dekoodataan etunimet ja sukunimi käsin, koska shibboleth ASCII-enkoodatut request headerit UTF-8 -merkistössä
                 Charset windows1252 = Charset.forName("Windows-1252");
                 Charset utf8 = Charset.forName("UTF-8");
                 etunimet = new String(etunimet.getBytes(windows1252), utf8);
