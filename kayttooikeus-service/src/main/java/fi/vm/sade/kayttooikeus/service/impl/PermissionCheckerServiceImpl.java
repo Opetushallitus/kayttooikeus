f6d0b2cb73a62513b7f987e1f4073c0cfa812b6c
Petri Ryhänen <petri.ryhanen@csc.fi>
2018-12-14 11:41:10 +0200

    KJHH-1277: Document KOOSTEROOLIENHALLINTA a bit. Remove KAYTTOOIKEUS_SCHEDULE.

--- kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/service/impl/PermissionCheckerServiceImpl.java
+++ kayttooikeus-service/src/main/java/fi/vm/sade/kayttooikeus/service/impl/PermissionCheckerServiceImpl.java
@@ -48,6 +48,7 @@ public class PermissionCheckerServiceImpl implements PermissionCheckerService {
     private static CachingRestClient restClient = new CachingRestClient().setClientSubSystemCode("henkilo.authentication-service");
     private static ObjectMapper objectMapper = new ObjectMapper();
     public static final String ROLE_KAYTTOOIKEUS_PREFIX = "ROLE_APP_KAYTTOOIKEUS_";
+    public static final String ROLE_KOOSTEROOLIENHALLINTA_PREFIX = "ROLE_APP_KOOSTEROOLIENHALLINTA"; // Käyttöoikeusryhmien katselu ja muokkaus
     public static final String PALVELU_KAYTTOOIKEUS = "KAYTTOOIKEUS";
     public static final String ROLE_REKISTERINPITAJA = "REKISTERINPITAJA";
     public static final String ROLE_CRUD = "CRUD";
