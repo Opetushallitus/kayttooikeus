8b6a2cfee8d9bf7d97f53828f1e1ea2bcda0e7b3
SeveriS <severi.suominen@gofore.com>
2019-04-03 10:31:03 +0300

    KJHH-1567: Muutetaan manuaalinen yksilöinti käyttöoikeuden nimeä

---
+++ kayttooikeus-service/src/main/resources/db/migration/V20190403100630401__update_ manuaalinenyksilointi_kayttooikeus_text.sql
@@ -0,0 +1,5 @@
+BEGIN;
+  UPDATE text SET text = 'Hetuttoman manuaalinen yksilöinti' WHERE textgroup_id = (
+    SELECT textgroup_id FROM kayttooikeus WHERE rooli = 'MANUAALINEN_YKSILOINTI' AND palvelu_id = (SELECT id FROM palvelu WHERE name = 'OPPIJANUMEROREKISTERI')
+  );
+COMMIT;
